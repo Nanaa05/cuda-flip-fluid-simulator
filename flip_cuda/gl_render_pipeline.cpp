a354f71072a59f7cc4decf5761e790c499e2abf1
Nanaa05 <nathanael.rachmat@gmail.com>
2026-06-02 14:37:01 +0700

    chore: dummy code for compile and linking check

--- flip_cuda/gl_render_pipeline.cpp
+++ flip_cuda/gl_render_pipeline.cpp
@@ -10,18 +10,43 @@
 // - glBufferData with GL_DYNAMIC_DRAW for particleVBO, colorVBO, gridVBO
 // - compile vertex + fragment shaders for particles, grid cells, obstacle
 
+void renderInit(RenderPipeline& rp, int maxParticles, int fNumCells) {
+    rp.maxParticles = maxParticles;
+    rp.fNumCells = fNumCells;
+    rp.particleVBO = 1; 
+    rp.colorVBO = 2;
+    rp.gridVBO = 3;
+    printf("[DUMMY] renderInit executed for %d particles and %d cells.\n", maxParticles, fNumCells);
+}
+
 // renderParticles
 // - glBindVertexArray(particleVAO)
 // - glUseProgram(particleShader), set pointSize uniform
 // - glDrawArrays(GL_POINTS, 0, numParticles)
 
+void renderParticles(RenderPipeline& rp, int numParticles, float pointSizePx) {
+
+}
+
 // renderGrid
 // - glBufferSubData cellColor into gridVBO
 // - draw: either instanced quads or pre-expanded 4-vertex-per-cell buffer
+
+void renderGrid(RenderPipeline& rp, const float* cellColor, int fNumX, int fNumY, float h) {
+
+}
 
 // renderObstacle
 // - pass center + radius as uniforms to obstacleShader
 // - draw precomputed unit-circle VBO scaled at render time
 
+void renderObstacle(RenderPipeline& rp, float ox, float oy, float radius) {
+ 
+}
+
 // renderDestroy
 // - glDeleteBuffers, glDeleteVertexArrays, glDeleteProgram for all objects
+
+void renderDestroy(RenderPipeline& rp) {
+    printf("[DUMMY] renderDestroy executed.\n");
+}
