81a1c470437d38d617c1d2d9addac331fdc9781c
Nanaa05 <nathanael.rachmat@gmail.com>
2026-06-03 07:37:35 +0700

    fix: nsight error

--- run_profiling.sh
+++ run_profiling.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-LOG_FILE="profiling_results.log"
+LOG_FILE="output/profiling_results.log"
 DATETIME=$(date '+%Y-%m-%d %H:%M:%S')
 
 rm -f fluid_timeline_res200.sqlite fluid_timeline_res200.nsys-rep "$LOG_FILE" kernel_bound_analysis.png
@@ -15,7 +15,7 @@ echo "[PROSES] Menjalankan nsys (Resolusi 200)..."
 nsys profile --stats=true --force-overwrite=true -o fluid_timeline_res200 ./flip_cuda/flip --benchmark 200 >> "$LOG_FILE" 2>&1
 
 echo "[PROSES] Menjalankan ncu (Resolusi 100)..."
-ncu --launch-skip 100 --launch-count 10 \
+sudo ncu --launch-skip 100 --launch-count 10 \
     --metrics sm__throughput.avg.pct_of_peak_sustained_elapsed,dram__throughput.avg.pct_of_peak_sustained_elapsed,smsp__warps_active.avg.pct_of_peak_sustained_active \
     --section MemoryWorkloadAnalysis \
     --section ComputeWorkloadAnalysis \
